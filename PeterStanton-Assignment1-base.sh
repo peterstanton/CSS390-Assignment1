c5686679f600174b1e300947067758e2633395a0
Peter Stanton <peterstanton@users.noreply.github.com>
2017-10-10 21:12:27 -0700

    Update PeterStanton-Assignment1-base.sh
    Pattern update.

--- PeterStanton-Assignment1-base.sh
+++ PeterStanton-Assignment1-base.sh
@@ -1,10 +1,10 @@
-echo -e "Total Tracks: $(find . -type f | grep ogg | wc -l) \n"
+echo -e "Total Tracks: $(find . -type f | grep \.ogg | wc -l) \n"
 
 
-echo -e "Total Artists: $(find . -type f | grep ogg | cut -f3 -d '/' | sort -u | wc -l) \n"
+echo -e "Total Artists: $(find . -type f | grep \.ogg | cut -f3 -d '/' | sort -u | wc -l) \n"
 
 
-echo -e "Multi-Genre Artist: \n $(find . -type f | grep ogg | cut -f2,3 -d '/' | sort -u | cut -f2 -d '/' | sort | uniq -d) \n"
+echo -e "Multi-Genre Artist: \n $(find . -type f | grep \.ogg | cut -f2,3 -d '/' | sort -u | cut -f2 -d '/' | sort | uniq -d) \n"
 
 
-echo -e "Multi-Disk Albums: \n $(find . -type f | grep ogg | cut -f4,5,6 -d '/' | grep [dD]isk\s*[1-9]/ | cut -f1 -d '/' | sort -u) \n"
+echo -e "Multi-Disk Albums: \n $(find . -type f | grep \.ogg | cut -f4,5,6 -d '/' | grep [dD]isk\s*[1-9]/ | cut -f1 -d '/' | sort -u) \n"
